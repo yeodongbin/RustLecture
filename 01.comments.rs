d31c96b415f12827dc04f018e71a4dc6560fae01
Yeo, Dongbin <yeodongbin2011@gmail.com>
2022-10-20 02:37:16 +0900

    Create 01.comments.rs

---
+++ 01.comments.rs
@@ -0,0 +1,14 @@
+fn main() {
+    // This is an example of a line comment
+    
+    /* 
+     * This is another type of comment, a block comment. In general,
+     */
+
+    /*
+       This is another type of comment
+    */
+
+    let x = 5 + /* 90 + */ 5;
+    println!("Is `x` 10 or 100? x = {}", x);
+}
